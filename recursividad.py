8adbe21cc93cf42894c43ce3d7ce8a922f1c8a96
sebastian <022100957k@uandina.edu.pe>
2025-03-05 12:40:20 -0500

    Conflicto resueltp

--- recursividad.py
+++ recursividad.py
@@ -5,11 +5,11 @@ def es_palindromo_recursivo(palabra, inicio, fin):
         return False
     return es_palindromo_recursivo(palabra, inicio + 1, fin - 1)  # Llamada recursiva reduciendo los índices
 
-def es_palindromo(palabra):
+def es_palindromoo(palabra):
     palabra = palabra.lower().replace(" ", "")  # Convertir a minúsculas y eliminar espacios
     return es_palindromo_recursivo(palabra, 0, len(palabra) - 1)  # Llamar a la función recursiva
 
 # Pruebas
-print(es_palindromo("reconocer"))  # True
-print(es_palindromo("python"))     # False
-print(es_palindromo("Anita lava la tina"))  # True (ignora espacios y mayúsculas)
+print(es_palindromoo("reconocer"))  # True
+print(es_palindromoo("python"))     # False
+print(es_palindromoo("Anita lava la tina"))  # True (ignora espacios y mayúsculas)
